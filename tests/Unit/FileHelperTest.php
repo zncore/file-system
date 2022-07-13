8248ff95b7996acf4f105e30254e5033e501e771
Vitaliy <theyamshikov@yandex.ru>
2022-07-13 14:06:15 +0600

    upd

--- tests/Unit/FileHelperTest.php
+++ tests/Unit/FileHelperTest.php
@@ -2,10 +2,9 @@
 
 namespace ZnCore\Base\Tests\Unit;
 
-use ZnLib\Components\Byte\Helpers\ByteSizeFormatHelper;
+use ZnCore\Entity\Helpers\CollectionHelper;
 use ZnCore\FileSystem\Helpers\FindFileHelper;
 use ZnCore\FileSystem\Helpers\MimeTypeHelper;
-use ZnCore\Entity\Helpers\CollectionHelper;
 use ZnTool\Test\Asserts\DataAssert;
 use ZnTool\Test\Asserts\DataTestCase;
 
@@ -52,23 +51,5 @@ final class FileHelperTest extends DataTestCase
         $extensions = MimeTypeHelper::getExtensionByMime('application/json');
 
         $this->assertEquals('json', $extensions);
-    }
-
-    public function testSize()
-    {
-        $size = ByteSizeFormatHelper::sizeFormat(123);
-        $this->assertEquals('123 B', $size);
-
-        $size = ByteSizeFormatHelper::sizeFormat(1022475);
-        $this->assertEquals('998.51 KB', $size);
-
-        $size = ByteSizeFormatHelper::sizeFormat(56461789651);
-        $this->assertEquals('52.58 GB', $size);
-
-        $size = ByteSizeFormatHelper::sizeFormat(5646178965111111);
-        $this->assertEquals('5.01 PB', $size);
-
-        $size = ByteSizeFormatHelper::sizeFormat(5999999999999999999);
-        $this->assertEquals('5.2 EB', $size);
     }
 }
